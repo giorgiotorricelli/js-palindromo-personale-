1509c610b64b3c191b9dcfb39d6070b94026b09c
Giorgio Torricelli <torricelligiorgio7@gmail.com>
2026-03-24 16:07:02 +0100

    controllo se la parola inserita è lunga almeno 2 caratteri

--- scrupt.js
+++ scrupt.js
@@ -1,1 +1,8 @@
-console.log("ciao");
+//l'utente ci da una parola o un numero
+
+const parolaDaVerificare = prompt("Inserisci una parola (o un numero) per vedere se è un palindromo: ");
+
+//verifichiamo che la stringa abbia almeno una lunghezza maggiore di 1
+if (parolaDaVerificare.length < 2) {
+    console.log("Deve avere una lunghezza di almeno 2 caratteri, riprova");
+}
