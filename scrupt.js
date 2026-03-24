dc5a0e9b3c28fa2e3665580a9d9a03d913c8cccd
Giorgio Torricelli <torricelligiorgio7@gmail.com>
2026-03-24 16:20:13 +0100

    implementato sistema di verifica della lunghezza del valore inserito dall'utente

--- scrupt.js
+++ scrupt.js
@@ -1,8 +1,24 @@
-//l'utente ci da una parola o un numero
+//dichiaro la variabile
 
-const parolaDaVerificare = prompt("Inserisci una parola (o un numero) per vedere se è un palindromo: ");
+let parolaDaVerificare 
 
 //verifichiamo che la stringa abbia almeno una lunghezza maggiore di 1
-if (parolaDaVerificare.length < 2) {
-    console.log("Deve avere una lunghezza di almeno 2 caratteri, riprova");
+//dopo 5 tentativi errati il programma si stoppa
+for (let i = 0; i < 5; i++) {
+    //l'utente inserisce il valore
+    parolaDaVerificare = prompt("Inserisci una parola (o un numero) per vedere se è un palindromo: ");
+    
+    //controllo la lunghezza
+    if (parolaDaVerificare.length < 2) {
+        console.log("Deve avere una lunghezza di almeno 2 caratteri, riprova");
+    } else {
+        //se la lunghezza è giusta
+        break
+    }
+    
+    if (i === 4) {
+        //se hai superato i 5 tentativi
+        console.log("Hai superato il numero di tentativi, ricarica la pagina per riprovare");
+    }
+    
 }
