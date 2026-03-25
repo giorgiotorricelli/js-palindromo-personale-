2910bd8453d492edb7a69fbf84455910b75e43dd
Giorgio Torricelli <torricelligiorgio7@gmail.com>
2026-03-25 10:01:56 +0100

    usato .floor e .ceil per considerare i casi in cui la parola ha lettere dispari e implementato il ciclo for

--- scrupt.js
+++ scrupt.js
@@ -1,18 +1,20 @@
-//dichiaro la variabile
+//dichiaro le variabili
 
-let parolaDaVerificare 
+let verificaLunghezza;
+let parolaDaVerificare = ""
 
 //verifichiamo che la stringa abbia almeno una lunghezza maggiore di 1
 //dopo 5 tentativi errati il programma si stoppa
 for (let i = 0; i < 5; i++) {
     //l'utente inserisce il valore
-    parolaDaVerificare = prompt("Inserisci una parola (o un numero) per vedere se è un palindromo: ");
+    verificaLunghezza = prompt("Inserisci una parola (o un numero) per vedere se è un palindromo: ");
     
     //controllo la lunghezza
-    if (parolaDaVerificare.length < 2) {
+    if (verificaLunghezza.length < 2) {
         console.log("Deve avere una lunghezza di almeno 2 caratteri, riprova");
     } else {
         //se la lunghezza è giusta
+        parolaDaVerificare += verificaLunghezza
         break
     }
     
@@ -22,3 +24,24 @@ for (let i = 0; i < 5; i++) {
     }
     
 }
+
+    //utilizzo Math.floor() nel caso i cui i caratteri della parola siano dispari mi consideri la
+    //prima metà della parola senza la lettera centrale
+    const primaMetaDellaParola = parolaDaVerificare.slice(0, Math.floor(parolaDaVerificare.length / 2))
+    //utilizzo Math.ceil() nel caso i cui i caratteri della parola siano dispari mi consideri la
+    //seconda metà della parola senza la lettera centrale
+    const secondaMetaDellaParolaDaInvertire = parolaDaVerificare.slice(Math.ceil(parolaDaVerificare.length / 2))
+    //dichiaro la variabile assegnandole il tipo stringa altrimenti al primo ciclo for mi stampa undefined
+    let secondaMetaDellaParolaInvertita = ""
+
+    for (let i = secondaMetaDellaParolaDaInvertire.length -1; i > -1; i--)  {
+        secondaMetaDellaParolaInvertita += secondaMetaDellaParolaDaInvertire[i]
+    }
+
+    if (primaMetaDellaParola === secondaMetaDellaParolaInvertita) {
+        console.log(`${parolaDaVerificare} è un palindromo`);
+    } else {
+        console.log(`${parolaDaVerificare} non è un palindromo`);
+    }
+
+
